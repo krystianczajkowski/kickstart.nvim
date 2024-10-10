9f72fde8450ede0b8db410bae60c2c4a7836029f
Krystian Czajkowski <36819859+krystianczajkowski@users.noreply.github.com>
2024-10-10 15:40:14 +0200

    remap shtift-tab to select previous item

--- init.lua
+++ init.lua
@@ -771,7 +771,7 @@ require('lazy').setup({
           -- Select the [n]ext item
           -- ['<C-n>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
